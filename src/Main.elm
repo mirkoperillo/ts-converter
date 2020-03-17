3a76c25a4dd6d996eba39dd6d933b6e49c3574b3
mirkoperillo <mirko.perillo@gmail.com>
2020-03-17 14:14:08 +0100

    add license header to sources

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,24 @@
+{-
+    Copyright (C) 2020-present Mirko Perillo and contributors
+  
+    This file is part of ts-converter.
+  
+    ts-converter is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    ts-converter is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with ts-converter.  If not, see <http://www.gnu.org/licenses/>.
+
+-}
+
 module Main exposing (..)
-
--- Press buttons to increment and decrement a counter.
---
--- Read how it works:
---   https://guide.elm-lang.org/architecture/buttons.html
---
-
 
 import Browser
 import Html exposing (Html, button, div, text, input, label, span)
